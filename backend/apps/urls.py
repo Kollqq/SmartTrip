8b0e19b9c8a6fa33235f2bf777d5effcc6aa78f3
anton <antonbybrest@gmail.com>
2025-05-10 00:16:44 +0200

    Version1.3

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),  # <-- Добавлено
+    path('', index, name='index'),  # <-- добавили эту строчку
 ]
